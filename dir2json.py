300a1f5979d021243f980fb760cdac3357a0a44d
Olga Dmitricenko <o.dmitricenko@gmail.com>
2016-09-24 15:39:49 +0200

    added an empty line after every entry

--- dir2json.py
+++ dir2json.py
@@ -1,7 +1,8 @@
 """Import all file names in a specific dir and store it as JSON file """
 import os, json
 
-with open("./dir2json.txt", 'w') as img_file:
-    for file_in_dir in os.walk("./stereoviews_by_peter_elfelt"):
+with open("./dir2json.txt", 'a') as img_file:
+    for root, dirs, file_in_dir in os.walk("stereoviews_by_peter_elfelt/"):
         for filename in file_in_dir:
-            json.dump(filename, img_file, indent=4)
+            json.dump(filename, img_file, indent=2)
+            img_file.write('\n')
