e89d146fba0a7fcf948cf88d23e015ffff0f9a17
Olga Dmitricenko <o.dmitricenko@gmail.com>
2016-09-26 14:59:40 +0200

    almost working

--- names2dict.py
+++ names2dict.py
@@ -1,11 +1,13 @@
-import re
+import re, json
 olgas_dict = {}
 
-
-with open("./dir2json.txt", 'r+') as data:
+with open('./dir2json.txt', 'r+') as data:
     for line in data:
-        list_of_2 = line.split("_")
-        title = list_of_2[0].split("-")
-        img_number = ''.join(re.findall('\d+', list_of_2[1]))
-        olgas_dict[img_number] = title
-print(olgas_dict)
+        list_of_2 = line.split("--")
+        seq_num, img_title= list_of_2[0].split("_")
+        img_title_list = img_title.split("-")
+        dfi_img_number = ''.join(re.findall('\d+', list_of_2[1]))
+        author = "Peter Elfelt"
+        olgas_dict[dfi_img_number] = [seq_num, img_title_list, author, line]
+
+print olgas_dict
