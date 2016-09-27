58a43ebd816a3c88c61848a3b5ba99b64abcf1dd
Olga Dmitricenko <o.dmitricenko@gmail.com>
2016-09-27 16:40:59 +0200

    excluding macOS generated files

--- dir2json.py
+++ dir2json.py
@@ -1,8 +1,9 @@
 """Import all file names in a specific dir and store it as JSON file """
 import os, json
 
-with open("./dir2json.txt", 'wr+') as img_file:
-    for root, dirs, file_in_dir in os.walk("stereoviews_by_peter_elfelt/"):
-        for filename in file_in_dir:
+with open("./dir2json.txt", 'w') as img_file:
+    for root, dirs, file_in_dir in os.walk("stereoviews_by_peter_elfelt/", topdown=True):
+        olga = [filename for filename in file_in_dir if not filename[0] == (".")]
+        for filename in olga:
             json.dump(filename, img_file, indent=2)
             img_file.write('\n')
