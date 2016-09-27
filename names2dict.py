5736327a7d4e7c697e6fd1ded1dad2775d3d6956
Olga Dmitricenko <o.dmitricenko@gmail.com>
2016-09-27 17:20:16 +0200

    replaced regEx with use of string

--- names2dict.py
+++ names2dict.py
@@ -1,13 +1,15 @@
-import re, json
+import string
 olgas_dict = {}
 
 with open('./dir2json.txt', 'r+') as data:
     for line in data:
         list_of_2 = line.split("--")
-        seq_num, img_title= list_of_2[0].split("_")
+        seq_num, img_title = list_of_2[0].split("_")
         img_title_list = img_title.split("-")
-        dfi_img_number = ''.join(re.findall('\d+', list_of_2[1]))
-        author = "Peter Elfelt"
-        olgas_dict[dfi_img_number] = [seq_num, img_title_list, author, line]
+        #dfi_img_number = ''.join(re.findall('\d+', list_of_2[1]))
+        dfi_img_number = list_of_2[1].translate(list_of_2[1], string.digits)
+        int_seq = seq_num.translate(seq_num, string.digits)
+        olgas_dict[dfi_img_number] = [seq_num, img_title_list, "Peter Elfelt", line]
 
-print olgas_dict
+with open("amost_db.txt", 'w') as txt_file:
+     txt_file.write(str(olgas_dict))
